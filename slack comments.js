a72f101be45454fe6673436f13feffc13a84625d
Tenacious-G <gordona86550@gmail.com>
2018-04-08 12:31:19 +0100

    send initial files to my remote repo

---
+++ slack comments.js
@@ -0,0 +1,15 @@
+I want to start with the project but I completely dont know where to start. Any advices?
+
+3 replies
+
+Kiur 5 hours ago
+
+https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true
+
+Janne 1 hour ago
+
+Check out the errors the game gives in the console and start implementing those missing things first, you'll soon have something on screen
+
+Lukas 1 hour ago
+
+Thanks guys I’ll start from errors first.
